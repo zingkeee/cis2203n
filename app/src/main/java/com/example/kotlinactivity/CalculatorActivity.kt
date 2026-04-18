ea4b4c2f6fa38dd914541968115ca24dc9aed978
Neil Codera <neilcodera5@gmail.com>
2026-04-18 15:21:18 +0800

    task a1: added calculator variables

--- app/src/main/java/com/example/kotlinactivity/CalculatorActivity.kt
+++ app/src/main/java/com/example/kotlinactivity/CalculatorActivity.kt
@@ -7,14 +7,13 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CalculatorActivity : AppCompatActivity() {
+
+    var currentInput = ""
+    var firstOperand = 0.0
+    var currentOperator = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_calculator)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
